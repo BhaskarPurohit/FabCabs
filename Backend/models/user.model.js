7bb79a0878da5c9241f701c69e827053bc3e5218
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2024-12-20 01:03:40 +0530

    user model with functions for hashing password

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,22 +1,46 @@
-const mongoose = require("mongoose")
-
+const mongoose =require("mongoose")
+const bcrypt = require('bcrypt')
+const jwtSecret = process.env.JWT_SECRET
 const userSchema = new mongoose.Schema({
     fullname:{
         firstname:{
             type:String,
             required: true,
-            minlength:[3, 'First name must be at least 3 characters long!']
+            minlength:[3,'First name can not be smaller than 3 letters']
         },
         lastname:{
             type:String,
-            required:false,
+            required:false
         }
     },
     email:{
         type:String,
-        required: true,
+        required:true
     },
+
+    //socket ID will be later used to track the captain
     socketId:{
-        type:String
+        type:String,
+        required:true
     }
 })
+
+//making functions on userSchema
+
+//generate Auth Token function
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id}, jwtSecret)
+    return token
+}
+
+//compare password function
+userSchema.methods.commparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+//hash the password
+userSchema.methods.hashPassword = async function(password){
+    return await bcrypt.hash(password, 10)
+}
+
+const userModel = mongoose.model("User", userSchema)
