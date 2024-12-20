a3198c2cd9319b79c0e5bfbc72e864eed291dbf8
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2024-12-20 23:33:49 +0530

    create user and user controller

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,1 +1,23 @@
 const userModel = require("../models/user.model")
+const userService = require('../services/user.service')
+const { validationResult } = require('express-validator')
+
+//register new user here
+const registerUser = async function (req, res, next) {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const  { firstname, lastname, email, password } = req.body
+    const hashedPassword = await userModel.hashedPassword(password)
+
+    const user = await userService.createUser({
+        firstname,
+        lastname,
+        email,
+        password: hashedPassword
+    })
+}
+
+module.exports = registerUser 
