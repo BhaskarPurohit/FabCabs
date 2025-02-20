739f5d65bcdd8b9a28b31bfa522be24393e5b90c
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2025-02-20 13:24:57 +0530

    git ignore file

--- Backend/services/user.service.js
+++ Backend/services/user.service.js
@@ -1,5 +1,19 @@
 const userModel = require("../models/user.model")
 
-const createUser = async function (req, res){
+const createUser = async function ({ firstname, lastname,  email, password }){
+    //check if the user has passed required fields
+    if(!firstname || !password || !email){
+        throw new Error("All fields are required")
+    }
 
+    //creating user if the required fields are present
+    const user = userModel.create({
+        fullname:{
+            firstname,
+            lastname
+        },
+
+        email,
+        password
+    })
 }
